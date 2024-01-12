a44a515f0fc167509b7151299d9d9041fe843643
Nikola Zovko <nikolajoe95@gmail.com>
2024-01-12 16:48:56 +0100

    dsdafv

--- rj-calcu/src/components/ButtonBox.jsx
+++ rj-calcu/src/components/ButtonBox.jsx
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
 
+
+//import icon
+import { MdError } from "react-icons/md";
+
+
 /* import css */
 import "../components/ButtonBox.css";
 
@@ -7,124 +12,119 @@ import "../components/ButtonBox.css";
 import { IoBackspace } from "react-icons/io5";
 
 const ButtonBox = ({ resultatValue, setResultatValue }) => {
-  const [buttonValue, setButtonValue] = useState("");
 
-  const handleCalculate = () => {
-    try {
-      setResultatValue(eval(resultatValue).toString());
-    } catch (error) {
-      setResultatValue("Error");
-    }
-  };
+    const handleClick = (e) => {
+        if (resultatValue.length >= 16) {
+            setResultatValue(
+                <div className="erormsg">
+                    <span> <MdError className="icon-error" /> So Much Big Input</span>
+                </div>
+            );
+            return;
 
-  const handleBtnClick = (value) => {
-    if (value === "C") {
-      // Handle clear button
-      setButtonValue("");
-      setResultatValue("0");
-    } else if (value === "=") {
-      // Handle equal button
-      try {
-        const result = eval(buttonValue);
-        if (isNaN(result) || !isFinite(result)) {
-          // Handle non-numeric or infinite result (e.g., division by zero)
-          setResultatValue("Error");
-        } else {
-          setResultatValue(result.toString());
         }
-      } catch (error) {
-        // Handle other errors
-        setResultatValue("Error");
-      }
-      setButtonValue("");
-    } else if (typeof value === "function") {
-      // Handle other buttons
-      value();
-    } else {
-      // Handle other buttons
-      setButtonValue((prevValue) => prevValue + value);
+        if (resultatValue.charAt(0) === '0') {
+            resultatValue = resultatValue.slice(1, resultatValue.length)
+        }
+        setResultatValue(resultatValue.concat(e.target.name));
     }
-  };
 
-  console.log(buttonValue);
+    const clear = () => {
+        setResultatValue("0");
+    }
+    console.log(clear)
 
-  /* function reset button click */
-  const handleClear = () => {
-    setButtonValue("");
-  };
+    const backSpace = () => {
+        const newResultatValue = resultatValue.slice(0, -1);
+        setResultatValue(newResultatValue.length > 0 ? newResultatValue : "0");
+    };
 
-  /* function Handle backspace button click */
-  const handleBackspace = () => {
-    setButtonValue((prevDisplay) => prevDisplay.slice(0, -1));
-  };
+    const calculate = () => {
+        try {
+            resultatValue = eval(resultatValue).toString()
+            if (resultatValue.includes('.')) {
+                resultatValue = + eval(resultatValue);
+                resultatValue = resultatValue.toFixed(4).toString();
+                setResultatValue(resultatValue);
+            } else {
+                setResultatValue(eval(resultatValue).toString());
+            }
+        } catch (error) {
+            setResultatValue(
+                <div className="erormsg">
+                    <span>ERROR <MdError className="icon-error" /></span>
+                </div>
+            )
+        }
+    }
 
-  return (
-    <div className="buttons">
-      <button
-        onClick={() => handleBtnClick(handleClear)}
-        className="clear color"
-      >
-        Clear
-      </button>
-      <button
-        onClick={() => handleBtnClick(handleBackspace)}
-        className="backspace color"
-      >
-        <IoBackspace />
-      </button>
-      <button name="/" className="color" onClick={() => handleBtnClick("/")}>
-        /
-      </button>
-      <button name="7" onClick={() => handleBtnClick("7")}>
-        7
-      </button>
-      <button name="8" onClick={() => handleBtnClick("8")}>
-        8
-      </button>
-      <button name="9" onClick={() => handleBtnClick("9")}>
-        9
-      </button>
-      <button name="*" className="color" onClick={() => handleBtnClick("7")}>
-        *
-      </button>
-      <button name="4" onClick={() => handleBtnClick("5")}>
-        4
-      </button>
-      <button name="5" onClick={() => handleBtnClick("5")}>
-        5
-      </button>
-      <button name="6" onClick={() => handleBtnClick("6")}>
-        6
-      </button>
-      <button name="-" className="color" onClick={() => handleBtnClick("-")}>
-        -
-      </button>
-      <button name="1" onClick={() => handleBtnClick("1")}>
-        1
-      </button>
-      <button name="2" onClick={() => handleBtnClick("2")}>
-        2
-      </button>
-      <button name="3" onClick={() => handleBtnClick("3")}>
-        3
-      </button>
-      <button name="+" className="color" onClick={() => handleBtnClick("+")}>
-        +
-      </button>
-      <button name="0" onClick={() => handleBtnClick("0")}>
-        0
-      </button>
-      <button name="." className="color" onClick={() => handleBtnClick(".")}>
-        .
-      </button>
-      <button
-        onClick={() => handleBtnClick(handleCalculate)}
-        className="equal color"
-      >
-        =
-      </button>
-    </div>
-  );
+    return (
+        <div className="buttons">
+            <button
+                onClick={clear}
+                className="clear color"
+            >
+                Clear
+            </button>
+            <button
+                onClick={backSpace}
+                className="backspace color"
+            >
+                <IoBackspace />
+            </button>
+            <button name="/" className="color" onClick={handleClick}>
+                /
+            </button>
+            <button name="7" onClick={handleClick}>
+                7
+            </button>
+            <button name="8" onClick={handleClick}>
+                8
+            </button>
+            <button name="9" onClick={handleClick}>
+                9
+            </button>
+            <button name="*" className="color" onClick={handleClick}>
+                *
+            </button>
+            <button name="4" onClick={handleClick}>
+                4
+            </button>
+            <button name="5" onClick={handleClick}>
+                5
+            </button>
+            <button name="6" onClick={handleClick}>
+                6
+            </button>
+            <button name="-" className="color" onClick={handleClick}>
+                -
+            </button>
+            <button name="1" onClick={handleClick}>
+                1
+            </button>
+            <button name="2" onClick={handleClick}>
+                2
+            </button>
+            <button name="3" onClick={handleClick}>
+                3
+            </button>
+            <button name="+" className="color" onClick={handleClick}>
+                +
+            </button>
+            <button name="0" onClick={handleClick}>
+                0
+            </button>
+            <button name="." className="color" onClick={handleClick}>
+                .
+            </button>
+            <button
+                onClick={calculate}
+                className="equal color"
+            >
+                =
+            </button>
+        </div>
+    );
 };
 
 export default ButtonBox;
--- rj-calcu/src/components/Screen.jsx
+++ rj-calcu/src/components/Screen.jsx
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 /* import css */
 import "../components/Screen.css";
 
 const Screen = ({ resultatValue, setResultatValue }) => {
+  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem('theme')) || "gradientOne")
+  useEffect(() => {
+    localStorage.setItem("theme", JSON.stringify(theme))
+    document.documentElement.removeAttribute("class")
+    document.documentElement.classList.add(theme)
+  }, [theme])
   return (
     <div>
-      <div className="theme">
-        <span className="colorTheme">1</span>
-        <span className="colorTheme">2</span>
-        <span className="colorTheme">3</span>
+      <div className="themeSelector">
+        <span className={theme === "gradientOne" ? "gradientOne activeTheme" : "gradientOne"} onClick={() => setTheme("gradientOne")}></span>
+        <span className={theme === "gradientTwo" ? "gradientTwo activeTheme" : "gradientTwo"} onClick={() => setTheme("gradientTwo")}></span>
+        <span className={theme === "gradientThree" ? "gradientThree activeTheme" : "gradientThree"} onClick={() => setTheme("gradientThree")}></span>
       </div>
       <div className="screen">
         <p>{resultatValue}</p>
